99cc3d8d3070529f0277230c049904de2d736b7f
Akshat <Supergolu123@gmail.com>
2023-11-20 19:54:42 +0530

    add comments and more responsive

--- spotify.js
+++ spotify.js
@@ -1,4 +1,7 @@
 console.log("javascript");
+
+//declarations
+
 let songIndex=0;
 let audioElement = new Audio("06. Aaoge Jab Tum.mp3");
 let masterPlay = document.getElementById('masterPlay');
@@ -8,31 +11,33 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('Songitem'));
 let Timer=document.querySelector('#timer');
 let TimeStamp=document.querySelectorAll(".timestamp");
-
-console.log(TimeStamp)
-
+// console.log(TimeStamp)
 // Timer.innerText=`${audioElement.duration}/${progress}`;
-
-
+//songs path
 let songs=[
     {songName: "06. Aaoge Jab Tum", filePath:"06. Aaoge Jab Tum.mp3",coverPath:'logo.jpg'},
     {songName: "Tumse Hi", filePath:"02  Tumse Hi .mp3",coverPath:'logo.jpg'},
     {songName: "Tum Saath Ho", filePath:'03 Tum Saath Ho.mp3',coverPath:'logo.jpg'},
     {songName: "Apna-Bana-le", filePath:'Apna-Bana-le.mp3',coverPath:'logo.jpg'},
-    {songName: "Hoshwalon Ko Khabar Kya - Sarfarosh", filePath:'Hoshwalon Ko Khabar Kya - Sarfarosh.mp3',coverPath:'logo.jpg'},
-    {songName: "Just Chill - Maine Pyaar Kyun Kiya", filePath:'Just Chill - Maine Pyaar Kyun Kiya .mp3',coverPath:'logo.jpg'},
+    {songName: "Hoshwalon Ko Khabar Kya", filePath:'Hoshwalon Ko Khabar Kya - Sarfarosh.mp3',coverPath:'logo.jpg'},
+    {songName: "Just Chill - MPKK", filePath:'Just Chill - Maine Pyaar Kyun Kiya .mp3',coverPath:'logo.jpg'},
     {songName: "Pehli Nazar me", filePath:'Pehli Nazar me.mp3',coverPath:'logo.jpg'},
-    {songName: "Tu Hai To Mujhe Phir Aur Kya Chahiye", filePath:'Tu Hai To Mujhe Phir Aur Kya Chahiye.mp3',coverPath:'logo.jpg'},
+    {songName: "Phir Aur Kya Chahiye", filePath:'Tu Hai To Mujhe Phir Aur Kya Chahiye.mp3',coverPath:'logo.jpg'},
 ]
 // for(i=0;i<songs.length;i++){
     // console.log(songs[i].filePath);
     // }
+
     Timer.innerText=`0:00/5:23`;
+
+    //setting songs path for each element
     songItems.forEach((element, i) => {
          element.getElementsByTagName('img')[0].src = songs[i].coverPath;
          element.getElementsByClassName('songName')[0].innerText = songs[i].songName;
      })
 
+
+     //setting time stamp for each songs
      TimeStamp.forEach((element, i) => {
         let audio = new Audio(songs[i].filePath)
         audio.setAttribute('preload', "metadata")
@@ -46,7 +51,9 @@ let songs=[
         // element.innerText = `${parseInt(songs.songName[i].duration/60)}:${parseInt(songs.songName[i].duration%60)}`;
     })
 //audioElement.play();
-//handle play/pause click
+
+
+//handle play/pause click on tab
 masterPlay.addEventListener('click', () => {
         if (audioElement.paused || audioElement.currentTime <= 0) {
             audioElement.play();
@@ -78,7 +85,7 @@ masterPlay.addEventListener('click', () => {
     })
 //listen to events
 audioElement.addEventListener('timeupdate', ()=>{
-    //update seekbar
+    //update seekbar and changing songs with it with timer and also for timer
     progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
     Timer.innerText=`${parseInt(audioElement.currentTime/60)}:${parseInt(audioElement.currentTime%60)}/${parseInt(audioElement.duration/60)}:${parseInt(audioElement.duration%60)}`;
     myProgressBar.value = progress;
@@ -117,6 +124,8 @@ const makeAllPlays = ()=>{
 
     })
 }
+
+//adding pause/play on playlist
 Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
     element.addEventListener('click', (e)=>{
         if (audioElement.paused || audioElement.currentTime <= 0) {
@@ -143,6 +152,8 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
           }
         })
 })
+
+//adding tab next feature
 document.getElementById('next').addEventListener('click', ()=>{
     if(songIndex>=7)
     songIndex=0;
@@ -165,6 +176,8 @@ document.getElementById('next').addEventListener('click', ()=>{
 
 
 })
+
+//adding tab previous feature
 document.getElementById('previous').addEventListener('click', ()=>{
     if(songIndex <= 0)
     songIndex=songs.length-1;
